499aeb47f5ad85467e3d0d701507dc08bb5ef29e
Cheng-Lin <tyrian2250@gmail.com>
2025-10-08 01:38:27 +0000

    Fix CI/CD

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,30 @@
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
 
 module.exports = [
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './html/myApp/tsconfig.json',
-        sourceType: 'module',
+        project: "./html/myApp/tsconfig.json",
+        sourceType: "module",
       },
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
+      "@typescript-eslint": tsPlugin,
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
-      semi: 'error',
-      quotes: ['error', 'single'],
+      semi: "error",
+      quotes: ["error", "single"],
     },
   },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     rules: {
-      semi: 'error',
-      quotes: ['error', 'single'],
+      semi: "error",
+      quotes: ["error", "single"],
     },
   },
 ];
--- html/myApp/src/app/app.component.spec.ts
+++ html/myApp/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title \'myApp\'', () => {
+  it("should have as title 'myApp'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('myApp');
