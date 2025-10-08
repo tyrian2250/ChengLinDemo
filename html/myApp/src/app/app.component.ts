2aac570c6368b46cf6fda2a2c185bda8abe5dda9
Cheng-Lin <tyrian2250@gmail.com>
2025-10-08 01:37:19 +0000

    Fix CI/CD

--- html/myApp/src/app/app.component.ts
+++ html/myApp/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  title = 'myApp';
+  
   constructor() {}
 
   ngOnInit(): void {}
